2aa86a258e287e75527c5ae852425febd0c91646
HernandoNJ <hernandonj.unity@gmail.com>
2025-04-01 14:56:10 -0500

    start cell configured

--- Assets/Mar23/GridSystem.cs
+++ Assets/Mar23/GridSystem.cs
@@ -1,46 +1,57 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Mar23
 {
 public class GridSystem: MonoBehaviour
-{
-    public Vector2Int containerSize;
-    public Vector2Int startGrid;
+{ 
+    [Tooltip("Grid dimensions (use odd numbers for centered start cell)")]
+    public Vector2Int gridSize;
+    
+    public Vector2Int startCell;
     public Vector2Int itemSize;
     public GameObject itemPrefab;
-    public List<Vector2Int> availableGridList;
-    public List<Vector2Int> busyGridList;
+    public List<GameObject> items;
+    public List<Vector2Int> initialCells;
+    public List<Vector2Int> busyCells;
     
-    public bool IsGridBusy(Vector2Int position) => busyGridList.Contains(position);
-    public void AddGrid(Vector2Int position) => busyGridList.Add(position);
-    public void RemoveGrid(Vector2Int position) => busyGridList.Remove(position);
+    public bool IsGridCellFull(Vector2Int position) => busyCells.Contains(position);
+    public void AddGridCell(Vector2Int position) => busyCells.Add(position);
+    public void RemoveGridCell(Vector2Int position) => busyCells.Remove(position);
 
     private void Start()
     {
         itemSize = itemPrefab.GetComponent<Item>().itemSize;
         GenerateGrid();
+        SetStartGridCell();
     }
 
-    public void GenerateGrid()
+    private void GenerateGrid()
     {
-        for (int i = 0; i < containerSize.x; i++)
+        for (int i = 0; i < gridSize.x; i++)
         {
-            for (int j = 0; j < containerSize.y; j++)
+            for (int j = 0; j < gridSize.y; j++)
             {
-                Instantiate(itemPrefab, new Vector3(i * itemSize.x, j * itemSize.y, 0), Quaternion.identity);
-                availableGridList.Add(new Vector2Int(i,j));
+                var cellPosition = new Vector2Int(i, j);
+                var newItem = Instantiate(itemPrefab, new Vector3(i * itemSize.x, j * itemSize.y, 0), Quaternion.identity);
+                newItem.GetComponent<Item>().gridCellPosition = cellPosition;
+                items.Add(newItem);
+                initialCells.Add(cellPosition);
             }
         }
     }
 
-    public void SetStartGrid()
+    private void SetStartGridCell()
     {
-        var x = startGrid.x;
-        var y = startGrid.y;
-        startGrid = new Vector2Int(x/2, y/2);
-        
+        startCell = new Vector2Int((gridSize.x/2), (gridSize.y/2));
+        foreach (var item in items)
+        {
+            var tempItem = item.GetComponent<Item>();
+            if (tempItem.gridCellPosition != startCell) continue;
+            
+            item.GetComponent<Renderer>().material.color = Color.green;
+            break;
+        }
     }
 }
 }
--- Assets/Mar23/Item.cs
+++ Assets/Mar23/Item.cs
@@ -5,6 +5,11 @@ namespace Mar23
 public class Item: MonoBehaviour
 {
     public Vector2Int itemSize;
-    public Vector2Int itemIndex;
+    public Vector2Int gridCellPosition;
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(itemSize.x, itemSize.y, 1);
+    }
 }
 }
